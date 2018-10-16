4ef1168eb1741fa15be4d174e86ddc2b4253fb0a
Tomoya185-miyawaki <tomo16185@gmail.com>
2018-10-16 21:09:35 +0900

    first commit

---
+++ wp-sample-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WordPress Sample Plugin
+Plugin URI: https://github.com/Tomoya185-miyawaki/wp-sample-plugin
+Description: WordPress Plugin Sample build.
+Version: 1.0.0
+Author: Tomoya Miyawaki
+Author URI: https://github.com/Tomoya185-miyawaki/wp-sample-plugin
+License: GPLv2 or later
+*/
