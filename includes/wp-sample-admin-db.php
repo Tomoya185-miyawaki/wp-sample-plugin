3977800e12e2ea7f9da2fa4bde1a4aab25f07369
Tomoya185-miyawaki <tomo16185@gmail.com>
2018-12-04 20:57:45 +0900

    [update]サンプル一覧機能追加

--- includes/wp-sample-admin-db.php
+++ includes/wp-sample-admin-db.php
@@ -60,13 +60,56 @@ class Sample_Plugin_Admin_Db {
 	}
 
 	/**
+	 * Select data.
+	 *
+	 * @version 1.0.0
+	 * @since   1.0.0
+	 */
+	 public function get_list_options() {
+		global $wpdb;
+		$prepared = 'SELECT * FROM ' .$this->table_name . ' ORDER BY update_date DESC';
+		return $wpdb->get_results( $prepared );
+	 }
+
+	/**
 	 * Insert Post.
 	 *
 	 * @version 1.0.0
 	 * @since   1.0.0
 	 * @param   array $post
 	 */
-	public function insert_options( array $post ){
-		
+	public function insert_options( array $post ) {
+		global $wpdb;
+
+		$data = array(
+			'image_url'            => $post['sample-image-url'],
+			'image_alt'            => $post['sample-image-alt'],
+			'link_url'             => $post['sample-image-link'],
+			'open_new_tab'         => $post['sample-image-target'],
+			'insert_element_class' => $post['sample-element-class'],
+			'insert_element_id'    => $post['sample-element-id'],
+			'how_display'          => $post['sample-how-display'],
+			'filter_category'      => $post['sample-filter-category'],
+			'category_id'          => $post['sample-display-category'],
+			'register_date'        => date( 'Y-m-d H:i:s' ),
+			'update_date'          => date( 'Y-m-d H:i:s' )
+		);
+
+		$prepared = array(
+			'%s',
+			'%s',
+			'%s',
+			'%d',
+			'%s',
+			'%s',
+			'%s',
+			'%d',
+			'%d',
+			'%s',
+			'%s'
+		);
+
+		$wpdb->insert( $this->table_name, $data, $prepared );
+		return $wpdb->insert_id;
 	}
 }
